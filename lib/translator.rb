eec1d122ab3671ee907bcd7f983716014b187ba3
tiya adugna <tiyakefalee@gmail.com>
2020-04-10 23:12:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ emoticon = YAML.load_file(path)
  hash = {"emoticon" => {}, "emoticon_symbols" => {}}
  emoticon.each do |k|
    #hash[:emoticon]=[k]
-     hash[:emoticon_symbols] = 
+     hash[:emoticon_symbols] = [k] 
  end
    emoticon.each do |english, japanese|
      
