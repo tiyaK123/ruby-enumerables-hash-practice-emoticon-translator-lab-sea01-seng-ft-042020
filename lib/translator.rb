cd8ca7524427f2ee13f73466b0fdf120205a8054
tiya adugna <tiyakefalee@gmail.com>
2020-04-10 23:01:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  
+emoticon = YAML.load_file(path)
+ hash = {"emoticon" => {}, "emoticon_symbols" => {}}
+ 
+   emoticon.each do |english, japanese|
+     
+    hash["emoticon"][japanese[1]] = english  
+    hash["emoticon_symbols"][japanese[0]] =japanese[1]
+end
+ hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticons)
   # code goes here
+  hash = load_library(path)
+  result = hash["emoticon_symbols"][emoticons]
+  if hash["emoticon_symbols"][emoticons]
+    result
+  else
+     "Sorry, that emoticon was not found"
+   end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  # code goes 
+  hash = load_library(path)
+  result = hash["emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
